c682ea769f22eb3431693d4426e473467c0a778e
Fahsai <fahsai@antinode.co.th>
2021-03-06 15:53:25 +0700

    add user click outsideModal

--- src/App.js
+++ src/App.js
@@ -3,37 +3,46 @@ import './App.css'
 import { COLORS, IMAGES } from './constants';
 import { Card, Modal } from './components';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="container">
-          <div className="headerBar">
-            <a className="font-42">My Pokedex</a>
-          </div>
+const App = () => {
 
-          <div className="flexRow list">
-            <Card width="49%" />
-            <Card width="49%" />
-            <Card width="49%" />
-            <Card width="49%" />
-            <Card width="49%" />
-            <Card width="49%" />
-            <Card width="49%" />
-            <Card width="49%" />
-            <Card width="49%" />
-            <Card width="49%" />
-          </div>
+  const [showModal, setShowModal] = React.useState(false);
 
-          <div className="buttonAdd" style={{ backgroundColor: COLORS.Fire }}>
-            <a style={{ color: COLORS.Colorless }}>+</a>
-          </div>
-
-          <Modal />
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="headerBar">
+          <a className="font-42">My Pokedex</a>
         </div>
+
+        <div className="flexRow list">
+          <Card width="49%" />
+          <Card width="49%" />
+          <Card width="49%" />
+          <Card width="49%" />
+          <Card width="49%" />
+          <Card width="49%" />
+          <Card width="49%" />
+          <Card width="49%" />
+          <Card width="49%" />
+          <Card width="49%" />
+        </div>
+
+        <div
+          className="buttonAdd"
+          style={{ backgroundColor: COLORS.Fire }}
+          onClick={() => setShowModal(true)}
+        >
+          <a style={{ color: COLORS.Colorless }}>+</a>
+        </div>
+
+        <Modal
+          isShow={showModal}
+          onClose={() => { setShowModal(false) }}
+        />
       </div>
-    )
-  }
+    </div>
+  )
 }
+
 
 export default App
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,30 +1,44 @@
 import * as React from "react";
 import { COLORS, IMAGES } from '../constants';
 import { Card } from "../components";
+import useOutsideClick from "../utils/useOutsideClick";
 
 const Modal = (props) => {
-  const { } = props;
-  return (
-    <div className="modal">
-      <div className="modalContainer" style={{ backgroundColor: COLORS.Colorless }}>
-        <div style={{ padding: 20 }}>
-          <div className="inputContainer" style={{ borderColor: COLORS.Gray }}>
-            <input style={{ flex: 1, border: 'none' }} />
-            <img style={{ width: 44, height: 44 }} src={IMAGES.iconSearch} alt="Search" />
-          </div>
+  const { isShow, onClose } = props;
 
-          <div style={{ overflow: 'scroll', height: 600, paddingTop: 20 }}>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-          </div>
+  const ref = React.useRef();
 
+  useOutsideClick(ref, () => {
+    if (isShow === true) {
+      onClose()
+    }
+  });
+
+  if (isShow) {
+    return (
+      <div className="modal">
+        <div ref={ref} className="modalContainer" style={{ backgroundColor: COLORS.Colorless }}>
+          <div style={{ padding: 20 }}>
+            <div className="inputContainer" style={{ borderColor: COLORS.Gray }}>
+              <input placeholder="Find pokemon" />
+              <img style={{ width: 44, height: 44 }} src={IMAGES.iconSearch} alt="Search" />
+            </div>
+
+            <div style={{ overflow: 'scroll', height: 600, paddingTop: 20 }}>
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+            </div>
+
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null
+  }
 };
 
 export default Modal;
